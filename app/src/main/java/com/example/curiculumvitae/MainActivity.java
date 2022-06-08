0ae1f852b0dd8b6e4b1aa565d41762d6fb88ad5a
humaidi <humaidizakaria1998@gmail.com>
2022-06-08 16:50:22 +0700

    Implementasi WebView

--- app/src/main/java/com/example/curiculumvitae/MainActivity.java
+++ app/src/main/java/com/example/curiculumvitae/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.curiculumvitae;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnHubungi;
+    private WebView wvCV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHubungi = findViewById(R.id.btn_hubungi);
+        wvCV = findViewById(R.id.wv_cv);
+        String telepon = "tel:082324118692";
+
+        btnHubungi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent call = new Intent(Intent.ACTION_DIAL, Uri.parse(telepon));
+                startActivity(call);
+            }
+        });
+
+//        mengakses direktori file html
+        String uriFile = "file:///android_res/raw/cv.html";
+        wvCV.loadUrl(uriFile);
     }
 }
